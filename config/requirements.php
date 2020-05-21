1653ac09b21117c17637a57c59ebbf6f6b92cbed
batopa <batopa@gmail.com>
2020-05-21 12:05:22 +0200

    chore: make phpcs really happy

--- config/requirements.php
+++ config/requirements.php
@@ -35,6 +35,7 @@ if (!extension_loaded('intl')) {
  * You can remove this if you are confident you have proper version of intl.
  */
 if (version_compare(INTL_ICU_VERSION, '50.1', '<')) {
+    // phpcs:ignore
     trigger_error('ICU >= 50.1 is needed to use CakePHP. Please update the `libicu` package of your system.' . PHP_EOL, E_USER_ERROR);
 }
 
